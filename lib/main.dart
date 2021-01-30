69a042a6c5bea4ba1978321c3df637bfb7c6441a
Ronaldo Morais <ronaldo.morais@alctel.com.br>
2021-01-29 22:22:23 -0300

    First commit.

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // runApp(MyScaffold());
+  runApp(MaterialApp(
+    title: 'My App',
+    home: SafeArea(child: MyScaffold()),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class MyText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Hello World!!!',
+        textDirection: TextDirection.ltr,
+      ),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(color: Colors.blue[500]),
+        height: 56.0,
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            IconButton(
+                icon: Icon(Icons.menu),
+                tooltip: 'Navigation Menu',
+                onPressed: null),
+            Expanded(
+              child: title,
+            ),
+            IconButton(
+                icon: Icon(Icons.search), tooltip: 'Search', onPressed: null)
+          ],
+        ));
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Column(
+      children: <Widget>[
+        MyAppBar(
+          title: Text('Example title'),
+        ),
+        Expanded(
+          child: Center(
+            child: Text(
+              'Hello World.',
+              textDirection: TextDirection.ltr,
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+}
